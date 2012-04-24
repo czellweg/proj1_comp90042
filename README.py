5c313dc72d5a4701288fd91def6fd2928405fab5
Christof Zellweger <czellweg@gmail.com>
2012-04-24 14:52:46 +1000

    adding a repository readme

---
+++ README.py
@@ -0,0 +1,19 @@
+# Christof Zellweger
+# 502193
+# 2012-04-24
+
+'''
+Project 1: README
+
+This project contains a Java project named lucene-test and a Python project
+named index-compression.
+
+Lucene-test contains an executable jar file with which a postings-file can be
+generated which then is used in the index-compression module. See the README in
+that project for further details on how to use it.
+
+index-compression contains Python implementations of two compression
+algorithms, namely a Variable Byte Encoding method and an algorithm based on
+Elias' Gamma codes. The two modules 'indexcompression' and the test module
+'test_indexcompression' can both be executed as expected.
+'''
