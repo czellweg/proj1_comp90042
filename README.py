4d91cc05b7c483c0ef611a8c2676005b8d276d10
Christof Zellweger <czellweg@gmail.com>
2012-04-24 14:55:56 +1000

    slightly adjusted the readme

--- README.py
+++ README.py
@@ -10,7 +10,9 @@ named index-compression.
 
 Lucene-test contains an executable jar file with which a postings-file can be
 generated which then is used in the index-compression module. See the README in
-that project for further details on how to use it.
+that project for further details on how to use it. There is already a postings-
+file.txt in the index-compression project which is ready to be used and was in
+fact used to get the results for the discussion.
 
 index-compression contains Python implementations of two compression
 algorithms, namely a Variable Byte Encoding method and an algorithm based on
